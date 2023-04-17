102ce7451e39359b295a00c3f49d8b18a0a6c688
GBaris <baris@berkut.tech>
2023-04-17 15:54:51 +0300

    yönetim panelinde müşteri yorumlarının okunması için bir sekme eklendi

--- Presentation/Nop.Web/Plugins/Widgets.AskVendor/Data/Domain/CustomerQuestion.cs
+++ Presentation/Nop.Web/Plugins/Widgets.AskVendor/Data/Domain/CustomerQuestion.cs
@@ -9,7 +9,14 @@ public class CustomerQuestion : BaseEntity
     public int CustomerId { get; set; }
     public int VendorId { get; set; }
     public int ProductId { get; set; }
+    public string Subject { get; set; }
     public string Question { get; set; }
-    public DateTime CreatedOnUtc  { get; set; } = DateTime.Now;
-
+    public DateTime QuestionDate  { get; set; } = DateTime.Now;
+    public string Answer { get; set; }
+    public DateTime AnswerDate { get; set; }
+    public bool IsReadByVendor { get; set; } = false;
+    public bool IsReadByCustomer { get; set; } = false;
+    public bool IsAnswered { get; set; } = false;
+    public bool IsDeletedByCustomer { get; set; } = false;
+    public bool IsDeletedByVendor { get; set; } = false;
 }
